93b6d372f484deabdcdcce5e2b2d9c735b240c06
Sakari Mursu <sakari@mursu.dev>
2020-08-08 23:45:31 +0300

    Use absolute paths

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,5 +1,4 @@
-import { API_BASE } from './env';
+import { API_BASE } from 'constants/env';
 
 export const URL_CREATE_USER = `${API_BASE}/users`;
 export const URL_CREATE_EVENT = `${API_BASE}/events`;
-
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Monbot } from 'monbot';
-import { BOT_AUTH_TOKEN, ADMINS } from './constants/env';
-import { commands } from './commands';
+import { BOT_AUTH_TOKEN, ADMINS } from 'constants/env';
+import { commands } from 'commands';
 
 Monbot(BOT_AUTH_TOKEN, {
   admins: ADMINS,
