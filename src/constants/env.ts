3168bfed69bd9bbb80b110ce4c842ba687b31b0f
Sakari Mursu <sakari@mursu.dev>
2020-08-09 11:24:26 +0300

    Add missing env key to constants

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -12,3 +12,5 @@ export const BOT_AUTH_TOKEN: string = process.env.BOT_AUTH_TOKEN ?? '';
 export const ADMINS: string[] = process.env.ADMINS?.split(',') ?? [];
 
 export const API_BASE = process.env.API_BASE;
+
+export const ENV = process.env.ENV;
