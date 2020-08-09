54b74628bc33026be18e1668638d17da373dfdda
Sakari Mursu <sakari@mursu.dev>
2020-08-09 11:27:04 +0300

    Change logger to do pretty print only on dev

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,19 @@
 import pino from 'pino';
+import { ENV } from 'constants/env';
+
+const options =
+  ENV === 'development'
+    ? {
+        level: 'debug',
+        prettyPrint: {
+          colorize: true,
+          ignore: 'pid,hostname',
+        },
+      }
+    : {
+        level: 'info',
+      };
 
 export const logger = pino({
-  level: 'debug',
-  prettyPrint: {
-    ignore: 'pid,hostname',
-  },
+  ...options,
 });
