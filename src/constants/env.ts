a9f4cc6a300908bcf555dad27e3678a143b73df8
Sakari Mursu <sakari@mursu.dev>
2020-10-04 14:16:55 +0300

    Remove redundant urls

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -2,6 +2,4 @@ export const BOT_AUTH_TOKEN: string = process.env.BOT_AUTH_TOKEN ?? '';
 
 export const ADMINS: string[] = process.env.ADMINS?.split(',') ?? [];
 
-export const API_BASE = process.env.API_BASE;
-
 export const ENV = process.env.NODE_ENV;
